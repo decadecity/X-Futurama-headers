3e8cd18e468fac3848d6cfdc16b097dc73ee1d9a
Orde Saunders <orde@decadecity.net>
2009-10-12 15:47:44 +0000

    Doc and improvements

--- futurama.php
+++ futurama.php
@@ -1,6 +1,17 @@
 <?php
 
+/**
+ * This code is placed in the public domain.
+ */
+
 class Futurama {
+  /**
+   * Returns an array with a header and a quote
+   *
+   * @return \p array Header and quote ('X-[character]', quote)
+   *
+   * @throws Exception Unable to open data file.
+   */
   public static function get_header()
   {
     $handle = @fopen(dirname(__FILE__).'/futurama.csv','rb');
@@ -14,12 +25,15 @@ class Futurama {
       }
     }
     fclose($handle);
+    if (count($quote_list) == 0) {
+      $quote_list[] = array('Bender','Well, we\'re boned!');
+    }
     $quote = $quote_list[array_rand($quote_list)];
     $quote[0] = 'X-'.strtr($quote[0],' ','-');
     return $quote;
   }
 }
 
-if ($argv[0] == str_ireplace(dirname(__FILE__).'/','',__FILE__)) {
+if (@$argv[0] == str_ireplace(dirname(__FILE__).'/','',__FILE__)) {
   print_r(Futurama::get_header());
 }
--- futurama.py
+++ futurama.py
@@ -1,10 +1,18 @@
 import csv, os, random, sys
 
+"""
+Returns a tuple containing a header and a quote.
+
+This code is placed in the public domain.
+"""
+
 def get_header():
     quote_list = []
     for quote in csv.reader(open(sys.path[0]+'/futurama.csv')):
         if len(quote) == 2:
             quote_list.append(quote)
+    if len(quote_list) == 0:
+        quote_list.append(('Bender','Well, we\'re boned!'))
     quote = quote_list[random.randrange(0,len(quote_list))]
     return ('X-%s' % quote[0].replace(' ','-'),quote[1])
 
