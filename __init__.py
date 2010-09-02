e5cae08ee1f8e4e645879aa98fea4aac704f4f95
Orde Saunders <orde@decadecity.net>
2010-09-02 21:29:13 +0000

    Changes to run with Django's caching system.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
 import futurama
 import logging
+try:
+    from django.core.cache import cache
+except ImportError:
+    cache = None
 
 class AddQuote():
 
     def process_response(self, request, response):
         try:
-            quote = futurama.get_header()
+            quote = futurama.get_header(cache=cache)
             response[quote[0]] = quote[1]
         except IOError, e:
             try:
