1b14a6a582c354a76174a006960647b4021e065d
Orde Saunders <orde@decadecity.net>
2011-01-23 00:13:39 +0000

    Pythonic solution to the caching problem.

--- futurama.py
+++ futurama.py
@@ -3,32 +3,22 @@ import csv
 import os
 import random
 
+import logging
+
 """
 Returns a tuple containing a header and a quote.
 
 This code is placed in the public domain.
 """
 
-def get_header(cache=None):
-    quote_list = []
-    key = 'futurama://quote_list/'
-    if cache is not None:
-        try:
-            quote_list = cache.get(key, [])
-        except:
-            pass
-    if len(quote_list) == 0:
-      quote_list = []
-      for quote in csv.reader(open(os.path.dirname(__file__) + '/futurama.csv')):
-          if len(quote) == 2:
-              quote_list.append(quote)
-    if len(quote_list) == 0:
-        quote_list.append(('Bender', "Well, we're boned!"))
-    if cache is not None:
-        try:
-            cache.set(key, quote_list, 60*60*24)
-        except:
-            pass
+quote_list = []
+for quote in csv.reader(open(os.path.dirname(__file__) + '/futurama.csv')):
+    if len(quote) == 2:
+        quote_list.append(quote)
+if len(quote_list) == 0:
+    quote_list.append(('Bender', "Well, we're boned!"))
+
+def get_header():
     quote = quote_list[random.randrange(0,len(quote_list))]
     return ('X-%s' % quote[0].replace(' ', '-'), quote[1])
 
