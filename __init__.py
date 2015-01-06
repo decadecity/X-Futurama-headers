129b4607e88e3232f7288f3386d552091e69ab0d
Orde Saunders <orde@decadecity.net>
2015-01-06 16:07:24 +0000

    Cleanup.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import futurama
-import logging
 
+"""Django middleware to add the quote."""
 
 class AddQuote():
 
--- futurama.py
+++ futurama.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-import csv
-import os
-import random
-
-import logging
+from csv import reader as csv_reader
+from os.path import join as path_join, dirname as path_dirname
+from random import randrange as random_randrange
 
 """
 Returns a tuple containing a header and a quote.
@@ -12,16 +10,19 @@ This code is placed in the public domain.
 """
 
 quote_list = []
-with open(os.path.dirname(__file__) + '/futurama.csv') as quotes:
-    for quote in csv.reader(quotes):
+try:
+    for quote in csv_reader(open(path_join(path_dirname(__file__), 'futurama.csv'))):
         if len(quote) == 2:
             quote_list.append(quote)
+except IOError:
+    pass
 if len(quote_list) == 0:
     quote_list.append(('Bender', "Well, we're boned!"))
 
 def get_header():
-    quote = quote_list[random.randrange(0,len(quote_list))]
+    quote = quote_list[random_randrange(0,len(quote_list))]
     return ('X-%s' % quote[0].replace(' ', '-'), quote[1])
 
 if __name__ == '__main__':
     print(get_header())
+
