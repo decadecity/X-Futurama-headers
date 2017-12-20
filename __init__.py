6ade2e7e021670e765bd1cecdb97251a77dfb0b9
Orde Saunders <orde@decadecity.net>
2017-12-20 11:32:18 +0000

    Fixing for Django 2.0

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.utils.deprecation import MiddlewareMixin
+
 from . import futurama
 
 """Django middleware to add the quote."""
 
-class AddQuote():
+class AddQuote(MiddlewareMixin):
 
     def process_response(self, request, response):
         quote = futurama.get_header()
